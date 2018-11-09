eb13867d6bcad39f1e014e4d67d47937953b12f9
EnderPearl <1069442394@qq.com>
2018-11-09 21:14:15 +0800

    Alpha 1.0

--- socket_class.cpp
+++ socket_class.cpp
@@ -1,2 +1,4 @@
 #include"socket_class.h"
+#ifdef WIN_OS
 int Socket::m_count=0;
+#endif
--- socket_class.h
+++ socket_class.h
@@ -2,16 +2,24 @@
 #define _SOCKET_CLASS_H_
 #define WIN_OS
 #include<string>
+#include<cstring>
 #ifdef WIN_OS
 #include<winsock2.h>
+#pragma comment(lib,"libws2_32")
+#define cs closesocket
 #else
-
+#include<unistd.h>
+// #include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#define cs close
 #endif
-#pragma comment(lib,"libws2_32")  
 class Socket{
 	public:
 		Socket(){
 			using namespace std;
+			#ifdef WIN_OS
 			if(m_count==0){
 				WORD sockVersion=MAKEWORD(2,2);
 				WSADATA wsaData;
@@ -19,25 +27,30 @@ class Socket{
 					throw string("Socket::Socket:cannot start up the socket");
 			}
 			m_count++;
-    		if((m_socket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP))==INVALID_SOCKET)
+			#endif
+    		if((m_socket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP))==-1)
 				throw string("Socket::Socket:create socket failed");
 		}
 		Socket(const Socket&)=delete;
 		Socket(Socket &&other):m_socket(other.m_socket){
-			other.m_socket=INVALID_SOCKET;
+			other.m_socket=-1;
+			#ifdef WIN_OS
 			m_count++;
+			#endif
 		}
 		Socket& operator=(const Socket&)=delete;
 		Socket& operator=(Socket&&)=delete;
 		~Socket(){
-			closesocket(m_socket);
+			cs(m_socket);
+			#ifdef WIN_OS
 			if(--m_count==0)
 				WSACleanup();
+			#endif
 		}
 		Socket* accept(sockaddr_in &address){
-			int len=sizeof(sockaddr_in);
-			SOCKET s=::accept(m_socket,(sockaddr*)&address,&len);  
-			if(s==INVALID_SOCKET)  
+			ltype len=sizeof(sockaddr_in);
+			stype s=::accept(m_socket,(sockaddr*)&address,&len);
+			if(s==-1)
 				throw std::string("Socket::accept:failed");
 			return new Socket(s);
 		}
@@ -45,23 +58,31 @@ class Socket{
 			sockaddr_in sin;
 			sin.sin_family=AF_INET;
 			sin.sin_port=htons(host);
+			#ifdef WIN_OS
 			sin.sin_addr.S_un.S_addr=INADDR_ANY;
-			if(::bind(m_socket,(sockaddr*)&sin,sizeof(sin))==SOCKET_ERROR)
+			#else
+			sin.sin_addr.s_addr=INADDR_ANY;
+			#endif
+			if(::bind(m_socket,(sockaddr*)&sin,sizeof(sin))==-1)
 				throw std::string("Socket::bind:failed");
 		}
 		void connect(unsigned short host,const char *ip){
 			sockaddr_in sin;
 			sin.sin_family=AF_INET;
 			sin.sin_port=htons(host);
+			#ifdef WIN_OS
 			sin.sin_addr.S_un.S_addr=inet_addr(ip);
-			if(::connect(m_socket,(sockaddr*)&sin,sizeof(sin))==SOCKET_ERROR)
+			#else
+			sin.sin_addr.s_addr=inet_addr(ip);
+			#endif
+			if(::connect(m_socket,(sockaddr*)&sin,sizeof(sin))==-1)
 				throw std::string("Socket::connect:failed");
 		}
 		void connect(unsigned short host,const std::string &ip){
 			connect(host,ip.c_str());
 		}
 		void listen(int len){
-			if(::listen(m_socket,len)==SOCKET_ERROR)
+			if(::listen(m_socket,len)==-1)
 				throw std::string("Socket::listen:failed");
 		}
 		std::string recv(int maxlen){
@@ -83,10 +104,20 @@ class Socket{
 			send(str.c_str());
 		}
 	private:
+		#ifdef WIN_OS
+		typedef SOCKET stype;
+		typedef int ltype;
 		static int m_count;
-		SOCKET m_socket;
-		Socket(SOCKET s):m_socket(s){
+		#else
+		typedef int stype;
+		typedef socklen_t ltype;
+		#endif
+		stype m_socket;
+		Socket(stype s):m_socket(s){
+			#ifdef WIN_OS
 			m_count++;
+			#endif
 		}
 };
+#undef cs
 #endif
